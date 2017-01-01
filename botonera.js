5d247dafdd85516f1c07e29bcd3aaf955f5ff2e1
adrian <adrian.miranda.r@gmail.com>
2016-12-31 21:21:13 -0300

    like var plus +1

--- botonera.js
+++ botonera.js
@@ -5,13 +5,14 @@ window.addEventListener('load' , function () {
     var positivo = document.querySelector('#positivo');
     var negativo = document.querySelector('#negativo');
     var contador = document.querySelector('#contador');  
-    var likes =  contador + 1;
+    var likes =  0;
 
     positivo.addEventListener('click', function(){
         console.log('boton apretado')
-        for(likes = 0 ; likes <= 10 ; likes++){
-            console.log(likes)
-        };
+        document.querySelector('#contador')
+            console.log(likes ++)
+        
+        
         
     });
 
