83eed7bf95f5980cfb17ec52a9d987199fbf74b9
adrian <adrian.miranda.r@gmail.com>
2019-02-03 20:05:56 -0300

    variables insertion

--- botonera.js
+++ botonera.js
@@ -2,8 +2,9 @@
 
 window.addEventListener('load' , function () {
    
-   var contador = '#contador';
-   
+   var positivo = document.getElementById('#positivo');
+   var negativo = document.getElementById('#negativo');
+   var contador = document.getElementById('#contador');
    for(var i = 0 ; i <= 0 ; i++){
     if('#contador'== Number){
         document.querySelector('#contador'++)
