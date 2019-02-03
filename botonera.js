ee02b74f9379894c90ad05113368cc9c0d16afff
adrian <adrian.miranda.r@gmail.com>
2019-02-03 19:57:42 -0300

    start exercice

---
+++ botonera.js
@@ -0,0 +1,12 @@
+'use strict'
+
+window.addEventListener('load' , function () {
+   
+   var contador = '#contador';
+   
+   for(var i = 0 ; i <= 0 ; i++){
+    if('#contador'== Number){
+        document.querySelector('#contador'++)
+    }
+   };
+});
