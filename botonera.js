0a2a55a28c5d2a910c1b806565030a87f6bb38cd
adrian <adrian.miranda.r@gmail.com>
2019-02-04 23:42:03 -0300

    like var plus +1

--- botonera.js
+++ botonera.js
@@ -5,7 +5,7 @@ window.addEventListener('load' , function () {
     var positivo = document.querySelector('#positivo');
     var negativo = document.querySelector('#negativo');
     var contador = document.querySelector('#contador');  
-    var likes =  0;
+    var likes =  1;
 
     positivo.addEventListener('click', function(){
         console.log('boton apretado')
