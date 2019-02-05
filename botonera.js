dc17b6e9870fe21b015de1381a46cbad077c5a12
adrian <adrian.miranda.r@gmail.com>
2019-02-04 22:28:47 -0300

    new variable

--- botonera.js
+++ botonera.js
@@ -5,15 +5,14 @@ window.addEventListener('load' , function () {
     var positivo = document.querySelector('#positivo');
     var negativo = document.querySelector('#negativo');
     var contador = document.querySelector('#contador');  
-    var likes = contador + 1;
+    var likes =  + 1;
 
     positivo.addEventListener('click', function(){
         console.log('boton apretado');
-        contador.addEventListener
-        while(contador == Number){
-            likes++
+
+        
             console.log(likes)
-        }
+        
         
     });
 
