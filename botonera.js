f51533f66308413901d0e016071608b4cc774665
adrian <adrian.miranda.r@gmail.com>
2019-02-04 22:42:16 -0300

    progress

--- botonera.js
+++ botonera.js
@@ -5,14 +5,13 @@ window.addEventListener('load' , function () {
     var positivo = document.querySelector('#positivo');
     var negativo = document.querySelector('#negativo');
     var contador = document.querySelector('#contador');  
-    var likes =  + 1;
+    var likes =  contador + 1;
 
     positivo.addEventListener('click', function(){
-        console.log('boton apretado');
-
-        
+        console.log('boton apretado')
+        for(likes = 0 ; likes <= 10 ; likes++){
             console.log(likes)
-        
+        };
         
     });
 
