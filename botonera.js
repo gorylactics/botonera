7e65b452dbdaf551ab2c9ca4ead61ed76bb94f13
adrian <adrian.miranda.r@gmail.com>
2019-02-10 14:41:17 -0300

    fix bug

--- botonera.js
+++ botonera.js
@@ -4,14 +4,18 @@ window.addEventListener('load' , function () {
    
     var positivo = document.querySelector('#positivo');
     var negativo = document.querySelector('#negativo');
-    var contador = document.querySelector('#contador' + 1);  
-    var likes =   1;
+    var contador = document.querySelector('#contador'+ 1);  
+    var likes = 1 + contador ;
 
     positivo.addEventListener('click', function(){
         // console.log('boton apretado')
         // console.log(contador ++)
        
-        
+        if(likes == 1){
+            console.log(' es cierto')
+        }else{
+            console.log('no es verdad' , contador++)
+        }
         
         
     });
